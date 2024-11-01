863f6b5339b50ec3390f3e03016da5922db6f390
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2024-11-01 19:16:17 +0100

    Uniformity

--- vectorize.py
+++ vectorize.py
@@ -29,12 +29,42 @@ def find_best_grid(matrix):
             for i in range(len(matrix)):
                 if matrix[i][j - 1] != matrix[i][j]:
                     score += 1
-        print(f'Grid size: {grid_size}: {score}')
+        print(f'Grid size {grid_size}: transition score {score}')
         if score > best_score:
             best_score = score
             best_grid_size = grid_size
     return best_grid_size
 
+def calculate_uniformity(matrix, grid_size):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    total_uniformity = 0
+    cell_count = 0
+    for i in range(0, rows, grid_size):
+        for j in range(0, cols, grid_size):
+            cell_pixels = []
+            for x in range(i, min(i + grid_size, rows)):
+                for y in range(j, min(j + grid_size, cols)):
+                    cell_pixels.append(matrix[x][y])
+            majority_color = max(set(cell_pixels), key=cell_pixels.count)
+            uniformity = cell_pixels.count(majority_color) / len(cell_pixels)
+            total_uniformity += uniformity
+            cell_count += 1
+    return total_uniformity / cell_count
+
+def find_best_grid_size(matrix):
+    max_size = min(len(matrix), len(matrix[0])) // 2
+    best_uniformity = 0
+    best_grid_size = 1
+    for grid_size in range(2, max_size + 1):
+        uniformity = calculate_uniformity(matrix, grid_size)
+        print(f'Grid size {grid_size}: uniformity {uniformity}')
+        if uniformity > best_uniformity:
+            best_uniformity = uniformity
+            best_grid_size = grid_size
+    return best_grid_size, best_uniformity
+
 matrix = process_image_to_matrix('qr.png')
-print(find_best_grid(matrix))
+find_best_grid(matrix)
+find_best_grid_size(matrix)
 
